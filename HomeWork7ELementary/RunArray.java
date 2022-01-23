37950e63d55139da79eb986ea722b8363ecef286
vova-duck <90979748+vova-duck@users.noreply.github.com>
2022-01-24 01:15:03 +0200

    Add files via upload

---
+++ HomeWork7ELementary/RunArray.java
@@ -0,0 +1,34 @@
+package HomeWork7ELementary;
+
+import java.util.Arrays;
+
+public class RunArray {
+    public static void main(String[] args) {
+
+        ArrayToArrayList arrayToArrayList = new ArrayToArrayList(5);
+
+        arrayToArrayList.add("Буба");
+        arrayToArrayList.add("Пупа");
+        arrayToArrayList.add("Лупа");
+        arrayToArrayList.add("Биба");
+        arrayToArrayList.add("Боба");
+        arrayToArrayList.add("Дада");
+        arrayToArrayList.print();
+        System.out.println();
+        arrayToArrayList.add(3,"ЗАТЫЧКА");
+        arrayToArrayList.print();
+        System.out.println();
+        arrayToArrayList.delete(2);
+        arrayToArrayList.print();
+        System.out.println();
+        arrayToArrayList.delete("ЗАТЫЧКА");
+        arrayToArrayList.print();
+        System.out.println();
+        System.out.println(arrayToArrayList.get(8));
+        System.out.println(arrayToArrayList.get(2));
+        System.out.println(arrayToArrayList.getCount() + " столько элементов");
+
+
+    }
+}
+
